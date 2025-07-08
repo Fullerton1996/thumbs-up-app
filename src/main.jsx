7fc7749a925c372f9423c226c371bbfc3affc5a7
Anton Fullerton <antonfullerton@Antons-MacBook-Pro.local>
2025-07-08 19:02:39 +0100

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function Home() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '4rem'}}>
+      <h1>Welcome!</h1>
+      <p>Go to the <a href="/sleep">Sleep Question</a> page.</p>
+    </div>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sleep" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
